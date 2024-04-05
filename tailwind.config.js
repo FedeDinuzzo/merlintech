8f8babc0ee91a1d5e3420eee7a7fcd5d8873eee2
Fede Di Nuzzo <Federicodinuzzo98@gmail.com>
2024-04-04 22:55:59 -0300

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,10 +19,9 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        primary: "#00040f",
-        secondary: "#00f6ff",
+        primary: "#ffffff",
+        secondary: "#000000",
         dimWhite: "rgba(255, 255, 255, 0.7)",
-        dimBlue: "rgba(9, 151, 124, 0.1)",
       },
     },
     screens: {
