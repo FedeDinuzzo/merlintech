825521cd7fc6a24dae7d6a107101ee80c03e83eb
Fede Di Nuzzo <federicodinuzzo98@gmail.com>
2024-04-16 01:56:02 -0300

    Contact

--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -5,43 +5,48 @@ import Link from "next/link"
 import { ButtonTwo } from "./Buttons"
 import { ButtonFour } from "./Buttons"
 import ChevronRight from "../../public/ChevronRight.svg"
-import cardInnerShadow from "../../public/card-inner-shadow.webp"
-// image bottom shadow
-import Card1Empresa from "../../public/Card1Empresa.png"
-import Card2Empresa from "../../public/Card2Empresa.png"
+import ShadowContact from "../../public/ShadowContact.png"
+import Disney from "../../public/TestimonialDisney.jpg"
+import Nike from "../../public/TestimonialNike1.jpg"
+import Nike2 from "../../public/TestimonialNike2.jpg"
 
 
 const Contact = () => {
 
   return (
-    <section id="features" className={`min-h-[100vh] max-w-[1360px] mx-auto flex flex-col flex-wrap justify-center`}>
+    <section id="features" className={`min-h-[100vh] mx-auto flex flex-col justify-center`}>
       {/* <div className={`bg-wave`}></div> */}
       <div className={`flex justify-center`}>
         <div className={`bg-line-services`}></div>
-        <div className={`flex flex-col max-w-[1260px] mx-auto`}>
+        <div className={`flex flex-col mx-auto`}>
           <h1 className={`${styles.heading2} text-gradient max-w-[770px] h-[100%] mb-10`}>Agentes Disponibles. Contactanos y reserva tu lugar en merlintech. Tu negocio crece, nosotros crecemos con vos</h1>
           <ButtonFour image={ChevronRight} text={`COMENZAR CONSULTORIA`} link={''} />
         </div>
       </div>
 
-      <div className={`${styles.flexCenter} flex-wrap p-[20px] gap-[20px] m:p-[30px] l:p-[40px] m:gap-[10px] l:gap-[20px]`}>
-        <div className={`bg-card-service flex flex-col flex-nowrap h-[313px] w-[350px] m:h-[465px] m:w-[620px] gap-2.5 p-5 top-[50%] l:top-[-22px] `}>
-          <h3 className={`${styles.heading3} my-2.5`}>Disney</h3>
-          <p className={`${styles.paragraph} mb-2.5`}>DesignCode UI offers an expansive library of UI components, each meticulously crafted for functionality and aesthetics.a</p>
-          <Image src={Card1Empresa} alt="supportIT" width="240" height="99" />
-        </div>
 
-        <div className={`bg-card-service flex flex-col flex-nowrap h-[313px] w-[350px] m:h-[465px] m:w-[620px] gap-2.5 p-5 top-[50%] l:top-[-22px] `}>
-          <h2 className={`${styles.heading3} my-2.5`}>Nike Argentina</h2>
-          <p className={`${styles.paragraph} mb-2.5`}>Our platform provides real-time guidance and advice on UI design best practices whether you're working on layout optimization, color scheme selection, or typography.</p>
-          <div className={`flex`}>
-            <Image src={Card2Empresa} alt="supportIT" width="240" height="99" />
-            <Image src={Card2Empresa} alt="supportIT" width="240" height="99" />
+      <div className={`${styles.flexCenter} flex-wrap p-[20px] gap-[20px] m:p-[30px] m:gap-[10px] l:gap-[20px]`}>
+        <div className={`bg-card-contact h-[465px] max-w-[620px] p-5`}>
+          {/* AGREGAR SOMBRA CON CSS */}
+          <Image className="absolute inset-0 w-full h-full object-cover rounded-[20px] z-10" src={ShadowContact} alt="Overlay" />
+          <h3 className={`${styles.heading3} text-[20px] mt-2.5 mb-2`}>Disney</h3>
+          <p className={`${styles.paragraph} mb-5 pr-32`}>DesignCode UI offers real-time guidance and advice on UI design best an expansive library of UI components, each meticulously crafted for functionality and aesthetics.a</p>
+          <div className={`flex justify-end absolute inset-0`}>
+            <Image className={`self-end rounded-br-[20px]`} src={Disney} alt="supportIT" width="603" height="305" />
           </div>
         </div>
 
-        <div className={`flex justify-evenly bg-card-service m:h-[194px] m:min-w-[1000px] l:w-[1260px] mx-auto`}>
+        <div className={`bg-card-contact h-[465px] max-w-[620px] p-5`}>
+          <h2 className={`${styles.heading3} text-[20px] mt-2.5 mb-2`}>Nike Argentina</h2>
+          <p className={`${styles.paragraph} mb-5 pr-32`}>Our platform provides real-time guidance and advice on UI design best practices whether you're working on layout optimization, color scheme selection, or typography.</p>
+          <div className={`flex justify-end absolute inset-0`}>
+            {/* EFECTO FONDO 1 - RECORTAR DE NUEVO */}
+            <Image className={`self-end`} src={Nike} alt="supportIT" width="245" height="266" />
+            <Image className={`self-end rounded-br-[20px]`} src={Nike2} alt="supportIT" width="362" height="320" />
+          </div>
+        </div>
 
+        <div className={`flex justify-evenly bg-card-contact m:h-[194px] m:min-w-[1000px] l:w-[1260px] mx-auto`}>
           <div className={`${styles.flexCenter}`}>
             <div className={`mx-2.5`}>
               <Image src={``} alt="supportIT" width="24" height="24" />
@@ -75,7 +80,6 @@ const Contact = () => {
           </div>
         </div>
       </div>
-
     </section >
   )
 }
