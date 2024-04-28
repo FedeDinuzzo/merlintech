9ecba26efb93473b0e49863c494173a04cedfad4
Fede Di Nuzzo <federicodinuzzo98@gmail.com>
2024-04-27 23:21:34 -0300

    public / He - he

--- src/app/components/Partners.js
+++ src/app/components/Partners.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import Image from 'next/image'
 import styles from '../constants/style'
-import SpiralBlur from '@/public/SpiralBlur.png'
-import hexagon0 from '@/public/hexagon0.png'
-import hexagon1 from '@/public/hexagon1.png'
-import hexagon2 from '@/public/hexagon2.png'
-import hexagon3 from '@/public/hexagon3.png'
-import hexagon4 from '@/public/hexagon4.png'
-import hexagon5 from '@/public/hexagon5.png'
-import hexagon6 from '@/public/hexagon6.png'
-import hexagon7 from '@/public/hexagon7.png'
-import hexagon8 from '@/public/hexagon8.png'
-import hexagon9 from '@/public/hexagon9.png'
-import hexagon10 from '@/public/hexagon10.png'
-import Ceo from '@/public/Ceo.png'
+import SpiralBlur from '../../public/SpiralBlur.png'
+import Hexagon0 from '../../public/Hexagon0.png'
+import Hexagon1 from '../../public/Hexagon1.png'
+import Hexagon2 from '../../public/Hexagon2.png'
+import Hexagon3 from '../../public/Hexagon3.png'
+import Hexagon4 from '../../public/Hexagon4.png'
+import Hexagon5 from '../../public/Hexagon5.png'
+import Hexagon6 from '../../public/Hexagon6.png'
+import Hexagon7 from '../../public/Hexagon7.png'
+import Hexagon8 from '../../public/Hexagon8.png'
+import Hexagon9 from '../../public/Hexagon9.png'
+import Hexagon10 from '../../public/Hexagon10.png'
+import Ceo from '../../public/Ceo.png'
 
 function Partners() {
   return (
@@ -35,23 +35,23 @@ function Partners() {
 
       <div className={`grid grid-cols-4 max-w-[96vw] m:max-w-[613px] z-50`}>
         <div className={`mt-[56%] -mx-[8px] m:-mx-3`}>
-          <Image className={`mb-3`} src={hexagon2} alt="Brands and Partners" width="175" height="151" />
-          <Image src={hexagon6} alt="Brands and Partners" width="175" height="151" />
+          <Image className={`mb-3`} src={Hexagon2} alt="Brands and Partners" width="175" height="151" />
+          <Image src={Hexagon6} alt="Brands and Partners" width="175" height="151" />
         </div>
         <div className={`-mx-[8px] m:-mx-3`}>
-          <Image className={`mb-3`} src={hexagon1} alt="Brands and Partners" width="175" height="151" />
-          <Image className={`mb-3`} src={hexagon4} alt="Brands and Partners" width="175" height="151" />
-          <Image src={hexagon8} alt="Brands and Partners" width="175" height="151" />
+          <Image className={`mb-3`} src={Hexagon1} alt="Brands and Partners" width="175" height="151" />
+          <Image className={`mb-3`} src={Hexagon4} alt="Brands and Partners" width="175" height="151" />
+          <Image src={Hexagon8} alt="Brands and Partners" width="175" height="151" />
         </div>
         <div className={`-mt-[56%] -mx-[8px] m:-mx-3`}>
-          <Image className={`mb-3`} src={hexagon0} alt="Brands and Partners" width="175" height="151" />
-          <Image className={`mb-3`} src={hexagon3} alt="Brands and Partners" width="175" height="151" />
-          <Image className={`mb-3`} src={hexagon7} alt="Brands and Partners" width="175" height="151" />
-          <Image src={hexagon10} alt="Brands and Partners" width="175" height="151" />
+          <Image className={`mb-3`} src={Hexagon0} alt="Brands and Partners" width="175" height="151" />
+          <Image className={`mb-3`} src={Hexagon3} alt="Brands and Partners" width="175" height="151" />
+          <Image className={`mb-3`} src={Hexagon7} alt="Brands and Partners" width="175" height="151" />
+          <Image src={Hexagon10} alt="Brands and Partners" width="175" height="151" />
         </div>
         <div className={`mt-[112%] -mx-[8px] m:-mx-3`}>
-          <Image className={`mb-3`} src={hexagon5} alt="Brands and Partners" width="175" height="151" />
-          <Image src={hexagon9} alt="Brands and Partners" width="175" height="151" />
+          <Image className={`mb-3`} src={Hexagon5} alt="Brands and Partners" width="175" height="151" />
+          <Image src={Hexagon9} alt="Brands and Partners" width="175" height="151" />
         </div>
       </div>
 
