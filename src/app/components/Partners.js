cb25d3987e094ded117773e07070024afb2891eb
Fede Di Nuzzo <federicodinuzzo98@gmail.com>
2024-04-30 16:28:31 -0300

    Iframe lazt

--- src/app/components/Partners.js
+++ src/app/components/Partners.js
@@ -27,7 +27,7 @@ function Partners() {
       </div>
 
       <div className={`z-0 absolute w-full h-[1440px]`}>
-        <iframe src='https://my.spline.design/distortedspiralcopy-d85ad473f4a28e665eaab8ae4f04118b/' frameborder='0' width='100%' height='100%'></iframe>
+        <iframe src='https://my.spline.design/distortedspiralcopy-d85ad473f4a28e665eaab8ae4f04118b/' frameborder='0' width='100%' height='100%' loading="lazy"></iframe>
       </div>
       <Image className={`absolute z-10 -mt-40 m:-mt-10 opacity-60 min-h-[800px]`} src={SpiralBlur} alt="gradient lights" width="1080" height="1080" />
       <div className={`absolute mt-10 z-0`}>
