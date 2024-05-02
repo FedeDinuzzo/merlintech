33c26310753a9ef1b3667ea4cddc970414062eae
Fede Di Nuzzo <federicodinuzzo98@gmail.com>
2024-05-01 21:41:51 -0300

    scroll

--- src/app/components/Scroll.js
+++ src/app/components/Scroll.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 import styles from '../constants/style'
 import ScrollCard from '../../public/ScrollBlur.png'
+import SupportRing from "../../public/SupportRing.svg"
 import ServicesCard from './ServicesCard'
 import { Button } from "./Buttons"
 import ChevronRight from "../../public/ChevronRight.svg"
@@ -9,28 +10,26 @@ import ChevronRight from "../../public/ChevronRight.svg"
 function Scroll() {
   return (
     <>
-      <section className={`min-h-[116vh] relative flex justify-between`}>
+      <section className={`min-h-[120vh] relative flex justify-between items-center`}>
         <div id="servicios" smooth={true} className={`absolute -mt-32`} />
-        {/* <Image className={`absolute flex`} src={ScrollCard} alt="" width="990" height="762" /> */}
-        <div className={`flex flex-col m:p-16 max-w-[582px]`}>
-          <h2 className={`${styles.heading}`}>
+        <div className={`m:mt-20 flex flex-col justify-center mx-5 m:mx-8 l:mx-24 max-w-[580px] z-50`}>
+          <h2 className={`${styles.heading} m:leading-[70px] mb-16`}>
             Simplifica
-            <span className={`inline text-white opacity-40`}>tus
+            <span className={`inline text-white opacity-40`}> tus
               operaciones con nuestro servicio de soporte IT</span>
           </h2>
 
-          <div className={``}>
-            <div className={``}>
-              <h3 className={``}>Servicios</h3>
-              <p className={``}>Nuestra gama de servicios de soporte IT está diseñada para abordar y resolver todos los desafíos tecnológicos de tu empresa.</p>
+          <div className={`m:min-w-[450px]`}>
+            <h3 className={`${styles.heading3White}`}>Servicios</h3>
+            <p className={`${styles.paragraph}`}>Nuestra gama de servicios de soporte IT está diseñada para abordar y resolver todos los desafíos tecnológicos de tu empresa.</p>
+
+            <div className={`my-10`}>
+              <h3 className={`${styles.heading3White} opacity-40`}>Destacados en Argentina</h3>
+              <p className={`${styles.paragraph} opacity-50`}>En Argentina y en el mundo, somos lideres en Soporte IT.</p>
             </div>
-            <div className={``}>
-              <h3 className={``}>Destacados en Argentina</h3>
-              <p className={``}>En Argentina y en el mundo, somos lideres en Soporte IT.</p>
-            </div>
-            <div className={``}>
-              <h3 className={``}>Sobre Nosotros</h3>
-              <p className={``}>Haz clic en la inicial, y descubri como llegamos a ser los mejores del sector.</p>
+            <div className={`mb-10`}>
+              <h3 className={`${styles.heading3White} opacity-40`}>Sobre Nosotros</h3>
+              <p className={`${styles.paragraph} opacity-50`}>Haz clic en la inicial, y descubri como llegamos a ser los mejores del sector.</p>
             </div>
           </div>
 
@@ -39,21 +38,16 @@ function Scroll() {
 
         <div className={`flex-none inset-0 overflow-hidden p-40 absolute z-40 min-h-[790px]`}>
           <div className={`absolute bg-radial-white h-[790px] z-40`}></div>
-          <div className={`absolute bg-radial-violet h-[600px] mt-[95px] ml-[5%] z-50`}></div>
+          <div className={`absolute bg-radial-violet h-[520px] mt-[150px] ml-[5%] z-50`}></div>
         </div>
 
-        <div className={`${styles.flexCenter}`}>
-          <div className={`relative my-12 z-50`}>
-            <div className={`${styles.flexCenter} min-h-[627px]`}>
-              <ServicesCard />
-            </div>
-          </div>
+        <div className={`absolute right-[0%] top-[89%] m:right-[10%] m:top-[82%] l:right-[40%] l:top-[45%] z-50`}>
+          <Image className={``} src={SupportRing} alt="progressRing" width="175" height="175" priority />
         </div>
-        <div className={`hidden`}>
 
-        </div>
-        <div className={`hidden`}>
-
+        <div className={`hidden m:flex h-[840px] relative z-50 mt-56`}>
+          <div class="w-full h-[260px] absolute bottom-0 bg-gradient-to-b from-transparent to-black rounded-[23px] z-50"></div>
+          <ServicesCard />
         </div>
       </section>
     </>
--- src/app/components/ServicesCard.js
+++ src/app/components/ServicesCard.js
@@ -35,64 +35,9 @@ import AvatarFox from '../../public/MainCard/AvatarFox.png'
 
 export default function ServicesCard() {
   return (
-    <section className="flex h-[627px] min-w-[375px] w-[1100px] max-w-[100vw] rounded-[23px] bg-gradient-to-b from-[#0000] to-[#0006]">
-      <div className={`bg-main-card border-[.75px] border-solid border-secondary border-opacity-10 flex flex-none flex-row flex-nowrap justify-start gap-15 inset-0 overflow-hidden p-0 absolute bg-[#050505e6] content-start items-start backdrop-blur-[7.5px]`}>
-        <div className={`min-w-[150px] max-w-[33vw] border-r-[.75px] border-solid border-white border-opacity-10 bg-black bg-opacity-10 flex flex-col justify-between h-full p-15 relative overflow-visible`}>
-          {/* COLUMN LEFT SIDE */}
-          <div className={`flex flex-col items-start justify-between p-[15px] gap-6 min-h-min-content overflow-visible relative w-full h-full`}>
-            {/* LOGO */}
-            <Image className="w-18 h-6 mt-[7.5px]" src={logo} alt="logo" priority />
-            {/* SERVICES */}
-            <div className={`-mt-20`}>
-              {/* SERVICES TOP */}
-              <div className={`flex`}>
-                <div className='-ml-1 m:ml-0 border-l-[2px] -mb-[6px] border-[#2670e9] border-solid'></div>
-                <div className='-ml-1 m:ml-0 absolute w-36 h-7 bg-gradient-to-r from-[#2670e9] to-transparent opacity-30'></div>
-                <Image className="pt-[6px] ml-[2px] m:ml-[13px]" src={MenuNineCircles} alt="" priority />
-                <p className={`${styles.headingMainCard} pt-[6px] pl-[9px]`}>Todos los Servicios</p>
-              </div>
-              <div className={`flex m:mx-[15px] my-[7.5px] space-y-4`}>
-                <Image className="mt-3" src={Hand} alt="" priority />
-                <p className={`${styles.headingMainCard} pl-[9px]`}>Soporte de PC</p>
-              </div>
-              <div className={`flex m:mx-[15px] my-[7.5px] space-y-4`}>
-                <Image className="mt-3" src={PatternLock} alt="" priority />
-                <p className={`${styles.headingMainCard} pl-[9px]`}>Redes y Servidores</p>
-              </div>
-              <div className={`flex m:mx-[15px] my-[7.5px] space-y-4`}>
-                <Image className="mt-3" src={CreditCard} alt="" priority />
-                <p className={`${styles.headingMainCard} pl-[9px]`}>Licencias de Software</p>
-              </div>
-              <div className={`flex m:mx-[15px] my-[7.5px] space-y-4`}>
-                <Image className="mt-3" src={PlayCircles} alt="" priority />
-                <p className={`${styles.headingMainCard} pl-[9px]`}>Backup en la Nube</p>
-              </div>
-              <div className={`flex m:mx-[15px] my-[7.5px] space-y-4`}>
-                <Image className="mt-3" src={Square} alt="" priority />
-                <p className={`${styles.headingMainCard} pl-[9px]`}>Servicio de Hosting</p>
-              </div>
-              <div className={`flex m:mx-[15px] my-[7.5px] space-y-4`}>
-                <Image className="mt-3" src={Photo} alt="" priority />
-                <p className={`${styles.headingMainCard} pl-[9px]`}>Camaras de Seguridad</p>
-              </div>
-            </div>
-            {/* SERVICES BOTTOM */}
-            <div className={`border-[.75px] border-solid border-white rounded-b-[10px] border-opacity-5 bg-black bg-opacity-10`}>
-              <div className={`flex m:mx-[15px] my-[7.5px] space-y-4`}>
-                <Image className="mt-3" src={Keyboard} alt="" priority />
-                <p className={`${styles.headingMainCard} pl-[9px]`}>Suministro Hardware</p>
-              </div>
-              <div className={`flex m:mx-[15px] my-[7.5px] space-y-4`}>
-                <Image className="mt-3" src={Settings} alt="" priority />
-                <p className={`${styles.headingMainCard} pl-[9px]`}>Proteccion y Seguridad</p>
-              </div>
-              <div className={`flex m:mx-[15px] my-[7.5px] space-y-4 mb-3`}>
-                <Image className="mt-3" src={Help} alt="" priority />
-                <p className={`${styles.headingMainCard} pl-[9px]`}>Centro de Ayuda</p>
-              </div>
-            </div>
-          </div>
-        </div>
+    <section className="flex h-[800px] min-w-[375px] w-[800px] max-w-[100vw] rounded-l-[23px]">
+      <div className={`bg-main-card border-[.75px] border-solid border-secondary border-opacity-10 flex flex-none flex-row flex-nowrap justify-start gap-15 inset-0 overflow-hidden p-0 absolute  content-start items-start backdrop-blur-[7.5px]`}>
+
         {/* TOP SEARCH */}
         <div className={`flex flex-col text-white w-[897px] h-full max-w-[65vw] p-[15px]`}>
           <Image className={`absolute h-full w-full -m-[15px] opacity-60`} src={cardInnerShadow} alt="supportIT" />
@@ -107,7 +52,7 @@ export default function ServicesCard() {
           <div className={`border-t-[1px] mt-[15px] border-secondary border-solid border-opacity-10`}></div>
           {/* TOP UBCATION */}
           <div className={`flex justify-between my-[15px] h-[34px] pr-3 m:pr-0`}>
-            <div className={`shadow-main-card flex justify-center items-center p-1 overflow-hidden border-[.75px] bg-[#0009] bg-opacity-5 rounded-[74px] border-secondary border-solid border-opacity-10 backdrop-blur-[7.5px]`}>
+            <div className={`shadow-main-card flex justify-center items-center p-1 overflow-hidden border-[.75px] rounded-[74px] border-secondary border-solid border-opacity-10 backdrop-blur-[7.5px]`}>
               <div className={`flex justify-between items-center w-[210px]`}>
                 <h4 className={`${styles.headingMainCard} px-2 leading-5 flex justify-center items-center overflow-hidden border-[.75px] bg-secondary bg-opacity-10 rounded-[74px] border-secondary border-solid border-opacity-10 backdrop-blur-[7.5px]`}>Ubicacion</h4>
                 <h4 className={`${styles.headingMainCard} ml-5`}>Baires</h4>
@@ -132,7 +77,7 @@ export default function ServicesCard() {
             {/* COLUMN ONE */}
             <div className={`flex flex-col space-y-4 w-full`}>
               {/* CARD ONE */}
-              <div className={`overflow-hidden z-20 flex h-[105px] max-w-[60vw] rounded-[10px] backdrop-blur-[15px] bg-[#0009] bg-opacity-60 border-[.75px] border-secondary border-solid border-opacity-10`}>
+              <div className={`overflow-hidden z-20 flex h-[105px] max-w-[60vw] rounded-[10px] backdrop-blur-[15px] border-[.75px] border-secondary border-solid border-opacity-10`}>
                 <div className={`flex w-full`}>
                   <div className={`flex flex-col w-1/2 justify-between space-y-2 max-h-[105px] p-2.5 py-2 m-2.5`}>
                     <h4 className={`${styles.headingMainCard} text-white font-medium`}>Horarios -&gt;</h4>
@@ -150,7 +95,7 @@ export default function ServicesCard() {
                 </div>
               </div>
               {/* CARD TWO */}
-              <div className={`overflow-hidden z-20 flex h-[144px] max-w-[60vw] rounded-[10px] backdrop-blur-[15px] bg-[#0009] bg-opacity-60 border-[.75px] border-secondary border-solid border-opacity-10`}>
+              <div className={`overflow-hidden z-20 flex h-[144px] max-w-[60vw] rounded-[10px] backdrop-blur-[15px] border-[.75px] border-secondary border-solid border-opacity-10`}>
                 <div className={`flex justify-between w-full`}>
                   <div className={`flex flex-col w-1/2 pl-2.5 m-2.5 space-y-1 m:space-y-2`}>
                     <Image className="-mb-3 w-14 -ml-2" src={SupportTool} alt="" priority />
@@ -160,30 +105,30 @@ export default function ServicesCard() {
                   </div>
                   <div className={`flex flex-col w-1/2 m-0 m:m-2.5 space-y-1`}>
                     <div className={`flex items-center mt-1`}>
-                      <div className={`flex text-[10px] px-2 py-[3px] mr-1 bg-black rounded-full`}>1</div>
+                      <div className={`flex text-[10px] px-2 py-[3px] mr-1 rounded-full`}>1</div>
                       <h4 className={`${styles.headingMainCard} opacity-80`}>Cyber Seguridad</h4>
                     </div>
                     <div className={`flex items-center`}>
-                      <div className={`flex text-[10px] px-2 py-[3px] mr-1 bg-black rounded-full`}>2</div>
+                      <div className={`flex text-[10px] px-2 py-[3px] mr-1 rounded-full`}>2</div>
                       <h4 className={`${styles.headingMainCard} opacity-80`}>Conectividad</h4>
                     </div>
                     <div className={`flex items-center`}>
-                      <div className={`flex text-[10px] px-2 py-[3px] mr-1 bg-black rounded-full`}>3</div>
+                      <div className={`flex text-[10px] px-2 py-[3px] mr-1 rounded-full`}>3</div>
                       <h4 className={`${styles.headingMainCard} opacity-80`}>Instalacion</h4>
                     </div>
                     <div className={`flex items-center`}>
-                      <div className={`flex text-[10px] px-2 py-[3px] mr-1 bg-black rounded-full`}>4</div>
+                      <div className={`flex text-[10px] px-2 py-[3px] mr-1 rounded-full`}>4</div>
                       <h4 className={`${styles.headingMainCard} opacity-80`}>Hardware</h4>
                     </div>
                     <div className={`flex items-center`}>
-                      <div className={`flex text-[10px] px-2 py-[3px] mr-1 bg-black rounded-full`}>5</div>
+                      <div className={`flex text-[10px] px-2 py-[3px] mr-1 rounded-full`}>5</div>
                       <h4 className={`${styles.headingMainCard} opacity-80`}>Software</h4>
                     </div>
                   </div>
                 </div>
               </div>
               {/* CARD THREE */}
-              <div className={`overflow-hidden z-20 flex w-full h-[162px] max-w-[60vw] rounded-[10px] backdrop-blur-[15px] bg-[#0009] bg-opacity-60 border-[.75px] border-secondary border-solid border-opacity-10`}>
+              <div className={`overflow-hidden z-20 flex w-full h-[162px] max-w-[60vw] rounded-[10px] backdrop-blur-[15px] border-[.75px] border-secondary border-solid border-opacity-10`}>
                 <div className={`flex justify-between px-2.5 py-1`}>
                   <Image className="w-10 -mt-16 mr-2" src={Stats} alt="" priority />
                   <div className={`flex flex-col p-2 space-y-3`}>
@@ -203,7 +148,7 @@ export default function ServicesCard() {
             {/* COLUMN TWO */}
             <div className={`hidden m:flex flex-col space-y-4 px-[15px] w-full`}>
               {/* CARD FOUR */}
-              <div className={`overflow-hidden z-20 flex w-full h-[342px] max-w-[60vw] rounded-[10px] backdrop-blur-[15px] bg-[#0009] bg-opacity-60 border-[.75px] border-secondary border-solid border-opacity-10`}>
+              <div className={`overflow-hidden z-20 flex w-full h-[342px] max-w-[60vw] rounded-[10px] backdrop-blur-[15px] border-[.75px] border-secondary border-solid border-opacity-10`}>
                 <div className={`flex flex-col p-[15px]`}>
                   <Image className="w-full mb-[6px]" src={ImageCard} alt="" priority />
                   <div className={`${styles.headingMainCard} my-1 bg-[#3395FF] rounded-md flex justify-center py-1 text-[13px] w-[154px]`}>ATENCION INMEDIATA</div>
@@ -223,76 +168,15 @@ export default function ServicesCard() {
                 </div>
               </div>
               {/* CARD FIVE */}
-              <div className={`overflow-hidden z-20 flex w-full h-[160px] max-w-[60vw] rounded-[10px] backdrop-blur-[15px] bg-[#0009] bg-opacity-60 border-[.75px] border-secondary border-solid border-opacity-10`}>
+              <div className={`overflow-hidden z-20 flex w-full h-[160px] max-w-[60vw] rounded-[10px] backdrop-blur-[15px] border-[.75px] border-secondary border-solid border-opacity-10`}>
                 <div className={`flex flex-col items-center w-full space-y-3`}>
                   <h4 className={`${styles.headingMainCard} text-medium pl-6 text-[13.5px] mt-[15px] `}>Monitoreo Permanente</h4>
                   <Image className="w-full px-6" src={Bars} alt="" priority />
                 </div>
               </div>
             </div>
-            {/* COLUMN THREE */}
-            <div className={`hidden l:flex flex-col space-y-4 w-full`}>
-              {/* CARD SIX */}
-              <div className={`overflow-hidden z-20 flex w-full h-[232px] max-w-[60vw] rounded-[10px] backdrop-blur-[15px] bg-[#0009] bg-opacity-60 border-[.75px] border-secondary border-solid border-opacity-10`}>
-                <div className={`flex flex-col py-[15px] px-[30px] space-y-2`}>
-                  <Image className="-ml-[15px] -mb-4" src={Toggle} alt="" priority />
-                  <h4 className={`${styles.headingMainCard} text-[13.5px] text-medium`}>Planes</h4>
-                  <p className={`${styles.headingMainCard} text-[11px] opacity-80`}>El mejor precio del mercado</p>
-                  <div className='border-[0.5px] border-secondary opacity-5 border-solid rounded'></div>
-                  <p className={`${styles.headingMainCard} text-[11px] opacity-80`}>Contamos con los mejores partners y resellers, ofreciendo un Soporte IT de categoria, a un precio competitivo, sin dudas la calidad esta asegurada</p>
-                  <div className='border-[0.5px] border-secondary opacity-5 border-solid rounded'></div>
-                  <div className={`flex items-center`}>
-                    <Image className="-ml-3" src={DISmall} alt="" priority />
-                    <p className={`${styles.headingMainCard} -mt-[18px] text-[11px] opacity-80`}>Diego Pineda, Creador de Merlin</p>
-                  </div>
-                </div>
-              </div>
-              {/* CARD SEVEN */}
-              <div className={`overflow-hidden z-20 flex flex-col h-[260px] max-w-[60vw] rounded-[10px] backdrop-blur-[15px] bg-[#0009] bg-opacity-60 border-[.75px] border-secondary border-solid border-opacity-10`}>
-                <div className={`flex justify-between pt-[15px] px-[15px]`}>
-                  <h4 className={`${styles.headingMainCard} text-medium text-[13.5px]`}>Notificaciones</h4>
-                  <div className={`flex justify-between -ml-28`}>
-                    <Image className="" src={home} alt="" priority />
-                    <Image className="" src={BarsHorizontal} alt="" priority />
-                    <Image className="ml-2 mt-[2px] h-5 w-5" src={AvatarChino} alt="" priority />
-                  </div>
-                </div>
-                <div className={`flex flex-col px-[15px] space-y-2`}>
-                  <div className={`flex w-full border-[1px] border-secondary border-opacity-10 rounded-md px-2 py-1`}>
-                    <Image className="mt-1 mr-2 h-6 w-6" src={AvatarFox} alt="" priority />
-                    <div className={`flex flex-col w-full`}>
-                      <h4 className={`${styles.headingMainCard} mr-[122px]`}>Gerente FOX</h4>
-                      <p className={`${styles.headingMainCard} opacity-80`}>Ticket resuelto</p>
-                    </div>
-                    <p className={`${styles.headingMainCard} opacity-80`}>2m ago</p>
-                  </div>
-                  <div className={`flex border-[1px] border-secondary border-opacity-10 rounded-md px-2 py-1`}>
-                    <Image className="mt-1 mr-2 h-6 w-6" src={AvatarUnicef} alt="" priority />
-                    <div className={`flex flex-col w-full`}>
-                      <h4 className={`${styles.headingMainCard} mr-[130px]`}>Ong Unicef</h4>
-                      <p className={`${styles.headingMainCard} opacity-80`}>Tecnico en camino</p>
-                    </div>
-                    <p className={`${styles.headingMainCard} opacity-80`}>5m ago</p>
-                  </div>
-                  <div className={`flex border-[1px] border-secondary border-opacity-10 rounded-md px-2 py-1`}>
-                    <Image className="mt-1 mr-2 h-6 w-6" src={AvatarDisney} alt="" priority />
-                    <div className={`flex flex-col w-full`}>
-                      <h4 className={`${styles.headingMainCard} mr-[123px]`}>Ceo Disney</h4>
-                      <p className={`${styles.headingMainCard} opacity-80`}>Servidores en linea</p>
-                    </div>
-                    <p className={`${styles.headingMainCard} opacity-80`}>15m ago</p>
-                  </div>
-                  <div className={`flex border-[1px] border-secondary border-opacity-10 rounded-md px-2 py-1`}>
-                    <Image className="mt-1 mr-2 h-6 w-6" src={AvatarNike} alt="" priority />
-                    <div className={`flex flex-col w-full`}>
-                      <h4 className={`${styles.headingMainCard} mr-[122px]`}>Manager Nike</h4>
-                      <p className={`${styles.headingMainCard} opacity-80`}>Mantenimiento activado</p>
-                    </div>
-                    <p className={`${styles.headingMainCard} opacity-80`}>1d ago</p>
-                  </div>
-                </div>
-              </div>
-            </div>
+
+
           </div>
         </div>
       </div >
