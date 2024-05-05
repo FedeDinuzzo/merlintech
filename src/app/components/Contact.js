2fa7688987f4d0e5d3d217f27dd4fd58fa852409
Fede Di Nuzzo <federicodinuzzo98@gmail.com>
2024-05-05 16:06:56 -0300

    contact cards shadow images

--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -30,19 +30,20 @@ const Contact = () => {
       <div className={`${styles.flexCenter} flex-wrap py-[20px] gap-[20px] m:py-[30px] m:gap-[10px] l:gap-[20px] max-w-[1380px] mx-auto`}>
         <div className={`bg-card-contact pt-5 pl-5 max-w-[590px] l:max-w-[620px]`}>
           {/* AGREGAR SOMBRA CON CSS */}
-          <Image className="absolute inset-0 w-full h-full object-cover rounded-[20px] z-10" src={ShadowContact} alt="Overlay" />
           <h3 className={`${styles.heading3} text-[20px] mt-2.5 mb-2`}>Disney</h3>
           <p className={`${styles.paragraph} mb-5 pr-5 m:pr-32 max-w-[620px]`}>DesignCode UI offers real-time guidance and advice on UI design best an expansive library of UI components, each meticulously crafted for functionality and aesthetics.a</p>
           <div className={`flex`}>
+            <Image className="absolute w-full h-full object-cover top-0 left-0 z-10" src={ShadowContact} alt="Overlay" />
             <Image className={`self-end rounded-br-[20px]`} src={Disney} alt="supportIT" width="603" height="305" />
           </div>
         </div>
 
         <div className={`bg-card-contact pt-5 pl-5 max-w-[590px] l:max-w-[620px]`}>
-          <Image className="absolute inset-0 w-full h-full object-cover rounded-[20px] z-10" src={ShadowContact} alt="Overlay" />
+
           <h3 className={`${styles.heading3} text-[20px] mt-2.5 mb-2`}>Nike Argentina</h3>
           <p className={`${styles.paragraph} mb-2 pr-5 m:pr-32 max-w-[620px]`}>Our platform provides real-time guidance and advice on UI design best practices whether youre working on layout optimization, color scheme selection, or typography.</p>
           <div className={`flex`}>
+            <Image className="absolute w-full h-full object-cover top-0 left-0" src={ShadowContact} alt="Overlay" />
             <Image className={`self-end w-2/5`} src={Nike} alt="supportIT" width="245" height="266" />
             <Image className={`self-end w-3/5 rounded-br-[20px]`} src={Nike2} alt="supportIT" width="362" height="320" />
           </div>
--- src/app/page.js
+++ src/app/page.js
@@ -15,10 +15,7 @@ export const metadata = {
 function Home() {
   return (
     <div className={`space-y-48`}>
-      <Hero
-        title="Soporte Tecnico IT"
-        title2="Para Empresas"
-      />
+      <Hero />
       <DynamicClients />
       <DynamicScroll />
       <DynamicPartners />
