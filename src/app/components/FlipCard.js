c396540f4df5ac92d120ee63481249abaa1ff891
Fede Di Nuzzo <federicodinuzzo98@gmail.com>
2024-05-08 01:44:42 -0300

    react memo & media query

--- src/app/components/FlipCard.js
+++ src/app/components/FlipCard.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 import { motion, useSpring } from "framer-motion";
+import { useMediaQuery } from "@react-hook/media-query";
 
 const spring = {
   type: "spring",
@@ -7,16 +8,16 @@ const spring = {
   damping: 40,
 };
 
-export function FlipCard({ width, height, itemsArray }) {
+export const FlipCard = React.memo(function FlipCard({ width, height, itemsArray }) {
   const [isFlipped, setIsFlipped] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [rotateXaxis, setRotateXaxis] = useState(0);
   const [rotateYaxis, setRotateYaxis] = useState(0);
+  const isMobile = useMediaQuery("(max-width: 810px)");
   const ref = useRef(null);
 
   const handleClick = () => {
     setIsFlipped((prevState) => !prevState);
-    // Cambiar al siguiente ítem en el array, reiniciar si se llega al final
     setCurrentIndex((prevIndex) => (prevIndex + 1) % itemsArray.length);
   };
 
@@ -47,48 +48,72 @@ export function FlipCard({ width, height, itemsArray }) {
     dx.set(-rotateXaxis);
     dy.set(rotateYaxis);
   }, [rotateXaxis, rotateYaxis]);
+  // rotateXaxis y rotateYaxis son los que cambian con el movimiento del ratón.
+  // No se agregan dx/dy porque no es necesario y causaría un ciclo de renderizado innecesario.
 
 
   return (
-    <motion.div
-      animate={{
-        rotateY: isFlipped ? -180 : 0,
-        scaleX: isFlipped ? -1 : 1,
-      }}
-      onClick={handleClick}
-      transition={spring}
-      style={{
-        perspective: "1200px",
-        transformStyle: "preserve-3d",
-        width: width,
-        height: height,
-      }}
-    >
-      <motion.div
-        ref={ref}
-        whileHover={{ scale: 1.05 }}
-        onMouseMove={handleMouseMove}
-        onMouseLeave={handleMouseEnd}
-        transition={spring}
-        style={{
-          width: "100%",
-          height: "100%",
-          rotateX: dx,
-          rotateY: dy,
-        }}
-      >
-        <div
+    <>
+      {isMobile ? (
+        <motion.div
+          onClick={handleClick}
+          animate={{
+            rotateY: isFlipped ? -180 : 0,
+            scaleX: isFlipped ? -1 : 1,
+            transition: spring,
+          }}
           style={{
             perspective: "1200px",
             transformStyle: "preserve-3d",
-            width: "100%",
-            height: "100%",
+            width,
+            height,
           }}
         >
-          {/* Renderiza el ítem actual del array */}
-          {itemsArray[currentIndex]}
-        </div>
-      </motion.div>
-    </motion.div>
+          <div style={{ width: "100%", height: "100%" }}>
+            {itemsArray[currentIndex]}
+          </div>
+        </motion.div>
+      ) : (
+        <motion.div
+          animate={{
+            rotateY: isFlipped ? -180 : 0,
+            scaleX: isFlipped ? -1 : 1,
+          }}
+          onClick={handleClick}
+          transition={spring}
+          style={{
+            perspective: "1200px",
+            transformStyle: "preserve-3d",
+            width: width,
+            height: height,
+          }}
+        >
+          <motion.div
+            ref={ref}
+            whileHover={{ scale: 1.05 }}
+            onMouseMove={handleMouseMove}
+            onMouseLeave={handleMouseEnd}
+            transition={spring}
+            style={{
+              width: "100%",
+              height: "100%",
+              rotateX: dx,
+              rotateY: dy,
+            }}
+          >
+            <div
+              style={{
+                perspective: "1200px",
+                transformStyle: "preserve-3d",
+                width: "100%",
+                height: "100%",
+              }}
+            >
+              {itemsArray[currentIndex]}
+            </div>
+          </motion.div>
+        </motion.div>
+      )}
+    </>
   );
-}
+});
