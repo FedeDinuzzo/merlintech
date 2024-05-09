7f63b55c12b23445f9a8221a5d83da9ad65db397
Fede Di Nuzzo <federicodinuzzo98@gmail.com>
2024-05-09 00:09:25 -0300

    without iframe

--- src/app/components/Partners.js
+++ src/app/components/Partners.js
@@ -1,3 +1,4 @@
+
 import React from 'react'
 import Image from 'next/image'
 import Spline from '@splinetool/react-spline'
@@ -29,7 +30,7 @@ function Partners() {
 
       <div className={`absolute w-full h-[1440px] -z-10`}>
         {/* <Spline className="w-full flex l:scale-[1.2] items-center justify-center md:justify-start" scene='https://prod.spline.design/4IeNjp3Pr8ziJ5bH/scene.splinecode' /> */}
-        <iframe src='https://my.spline.design/distortedspiralcopy-d85ad473f4a28e665eaab8ae4f04118b/' frameBorder='0' width='100%' height='100%' loading="lazy" ></iframe>
+        {/* <iframe src='https://my.spline.design/distortedspiralcopy-d85ad473f4a28e665eaab8ae4f04118b/' frameBorder='0' width='100%' height='100%' loading="lazy" ></iframe> */}
       </div>
 
       <div className='absolute w-full h-[1000px] -mt-32 m:-mt-48'>
