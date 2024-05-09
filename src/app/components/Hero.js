2bfb453c29b559b7e132369b1b1c2b153b43f353
Fede Di Nuzzo <federicodinuzzo98@gmail.com>
2024-05-09 00:36:20 -0300

    render

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -64,7 +64,7 @@ const Hero = () => {
 
           <div className="flex relative item">
             <div className={`bg-card-hero absolute flex flex-col flex-nowrap h-[380px] w-[340px] gap-2.5 p-5 -top-[330px] left-1/2 translate-x-[-50%] l:left-[88%] l:-top-[670px] z-40`}>
-              <Image src={cuposLimitados} alt="supportIT" width="1200" height="627" />
+              <Image src={cuposLimitados} alt="supportIT" width="1200" height="627" priority />
               <h2 className={`${styles.heading3} my-2.5`}>Soporte IT Personalizado</h2>
               <p className={`${styles.paragraph} mb-2.5`}>Asegure que sus datos y sistemas estén siempre disponibles, protegidos y actualizados.</p>
               <ButtonTwo image={ChevronRight} text="Realizar Auditoria" link="" />
