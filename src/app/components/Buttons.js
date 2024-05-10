58e3afb8dc38963a0509b7bdee580f8e47d3a5de
Fede Di Nuzzo <federicodinuzzo98@gmail.com>
2024-05-10 01:30:59 -0300

    btns images

--- src/app/components/Buttons.js
+++ src/app/components/Buttons.js
@@ -5,7 +5,7 @@ export const Button = ({ text, link, image }) => {
   return (
     <Link href={`/${link}`} aria-label={link}>
       <button type="button" className={`py-3 px-[30px] gap-x-3 bg-button font-inter font-medium text-[14px] text-secondary outline-none rounded-[10px] flex cursor-pointer`}>
-        {text} <Image src={image} className="fill-white mt-0.5 w-auto h-auto" alt="arrow" width="16" height="16" />
+        {text} <Image src={image} className="fill-white mt-0.5 w-auto h-auto" alt="arrow" width="16" height="16" priority />
       </button>
     </Link>
   );
@@ -15,7 +15,7 @@ export const ButtonTwo = ({ text, link, image }) => {
   return (
     <Link href={`/${link}`} aria-label={link}>
       <button type="button" className={`py-2 px-4 gap-x-2 border border-1 border-dimWhite font-inter font-medium text-[14px] text-dimWhite opacity-90 rounded-lg flex cursor-pointer`}>
-        {text} <Image src={image} className="fill-dimWhite opacity-70 mt-[3px] w-auto h-auto" alt="arrow" width="16" height="16" />
+        {text} <Image src={image} className="fill-dimWhite opacity-70 mt-[3px] w-auto h-auto" alt="arrow" width="16" height="16" priority />
       </button>
     </Link>
   );
