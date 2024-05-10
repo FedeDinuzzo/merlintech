bc892152b2a75291589dd5afddc61ce18dfba43d
Fede Di Nuzzo <federicodinuzzo98@gmail.com>
2024-05-10 01:42:39 -0300

    trying to fix

--- src/app/components/ServerIframe.js
+++ src/app/components/ServerIframe.js
@@ -1,13 +1,37 @@
-import React from 'react'
+"use client"
+import { useEffect, useState } from 'react';
 
 // Simulamos una función de solicitud del lado del servidor para obtener la URL del video
 async function getVideoSrc() {
-  // URL del video desde tu fuente de datos, puede ser una base de datos, una API externa, etc.
+  // Aquí deberías tener la lógica real para obtener la URL del video desde tu fuente de datos, como una base de datos o una API externa.
   return 'https://my.spline.design/distortedspiralcopy-d85ad473f4a28e665eaab8ae4f04118b/';
 }
 
-export default async function ServerIframe() {
-  const src = await getVideoSrc()
+export default function ServerIframe() {
+  const [src, setSrc] = useState(null);
 
-  return <iframe className="absolute w-full h-[1440px]" src={src} frameborder="0" allowfullscreen />
+  useEffect(() => {
+    const fetchVideoSrc = async () => {
+      try {
+        const videoSrc = await getVideoSrc();
+        setSrc(videoSrc);
+      } catch (error) {
+        console.error('Error al obtener la URL del video:', error);
+      }
+    };
+
+    fetchVideoSrc();
+  }, []);
+
+  return src ? (
+    <iframe
+      className="w-full"
+      src={src}
+      title="Video"
+      allowFullScreen
+      style={{ border: 'none' }} // Aplica estilos inline para eliminar el borde del iframe
+    />
+  ) : (
+    <p>Cargando video...</p>
+  );
 }
