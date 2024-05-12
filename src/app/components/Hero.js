e393afad21cb7d4ed80470184949151b15fc8345
Fede Di Nuzzo <federicodinuzzo98@gmail.com>
2024-05-12 16:27:04 -0300

    fix mouse cursor, hero btn

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -15,7 +15,7 @@ const Hero = () => {
     <section className={`flex flex-col flex-nowrap min-h-[100vh] justify-center`}>
       <div id="home" className={`absolute top-0`} />
 
-      <div className={`hidden l:flex absolute -left-3 top-[60px] bg-lines h-[1000px] w-[100vw]`}></div>
+      <div className={`hidden m:flex absolute -left-3 top-[60px] bg-lines h-[1000px] w-[100vw]`}></div>
 
       <div className={`flex-none inset-0 overflow-hidden absolute min-h-[1440px] opacity-90 l:opacity-100`}>
         <div className={`absolute bg-radial-white w-[760px] m:w-[1054px] top-[200px]`}></div>
@@ -41,15 +41,15 @@ const Hero = () => {
       </div>
 
       <div className={`${styles.flexCenter}`}>
-        <div className={`relative my-12 `}>
+        <div className={`relative my-12`}>
           <div className={`${styles.flexCenter} min-h-[627px] l:w-[1100px]`}>
             <div className={`flex-none inset-0 overflow-hidden absolute min-h-[20px] w-full`}>
               <div className={`absolute bg-lights w-[calc(100%-40px)] h-1`}></div>
             </div>
-            <div className="hidden m:flex ">
+            <div className="hidden m:flex">
               <MainCard />
             </div>
-            <div className={`absolute top-[-6%] right-[36px] m:right-0 m:left-[7%] m:-top-[13%] l:-left-[9%] z-50`}>
+            <div className={`z-10 absolute top-[-6%] right-[36px] m:right-0 m:left-[7%] m:-top-[13%] l:-left-[9%]`}>
               <RingOne />
             </div>
             <div className={`absolute right-[-165px] top-[92%] m:right-[10%] m:top-[82%] l:-right-[9%] l:top-[40%] z-50`}>
@@ -58,7 +58,7 @@ const Hero = () => {
           </div>
 
           <div className="-mt-[220px] l:mt-0 flex relative">
-            <div className={`bg-card-hero absolute flex flex-col flex-nowrap h-[380px] w-[340px] gap-2.5 p-5 -top-[330px] left-1/2 translate-x-[-50%] l:left-[88%] l:-top-[670px] z-40`}>
+            <div className={`bg-card-hero absolute flex flex-col flex-nowrap h-[380px] w-[340px] gap-2.5 p-5 -top-[330px] left-1/2 translate-x-[-50%] l:left-[88%] l:-top-[670px] `}>
               <Image src={cuposLimitados} width="300" height="123" alt="card" priority />
               <h2 className={`${styles.heading3} my-2.5`}>Soporte IT Personalizado</h2>
               <p className={`${styles.paragraph} mb-2.5`}>Asegure que sus datos y sistemas estén siempre disponibles, protegidos y actualizados.</p>
