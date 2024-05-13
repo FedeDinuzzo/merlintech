00bc1066da9ddaa56422888c6fea809e6c8514ff
Fede Di Nuzzo <federicodinuzzo98@gmail.com>
2024-05-12 21:15:17 -0300

    try m

--- src/app/components/ScrollSection/ScrollThree.js
+++ src/app/components/ScrollSection/ScrollThree.js
@@ -1,25 +1,14 @@
-import React from 'react'
-import Link from 'next/link'
-import styles from '../../constants/style'
-import ScrollText from './ScrollText'
-import ServicesCard from './ServicesCard'
-import { RingThree } from "../Ring"
+"use client"
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { Html } from '@react-three/drei';
+import Spline from '@splinetool/react-spline';
 
-function ScrollThree() {
+export default function ScrollThree() {
   return (
-    <>
-      <div className={`hidden m:flex h-[840px] relative`}>
-        <Link href="/servicios">
-          <div className="w-full h-[260px] absolute bottom-0 bg-gradient-to-b from-transparent to-black rounded-[23px] z-50"></div>
-          <div className={`flex-none inset-0 overflow-hidden absolute min-h-[20px] w-full`}>
-            <div className={`absolute bg-lights w-[calc(100%-48px)] h-1`}></div>
-          </div>
 
-          <ServicesCard />
-        </Link>
-      </div>
-    </>
-  )
+    <Spline scene="https://prod.spline.design/ShnmOlaePhgL49pm/scene.splinecode" />
+
+
+  );
 }
-
-export default ScrollThree
